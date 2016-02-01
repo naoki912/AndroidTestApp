946ffbbb088035d93484ac44a9f3ad9879adc319
Naoki <hnron.reg@gmail.com>
2016-02-01 10:12:03 +0900

    DirectoryArrayAdapterのリファクタリング

--- app/src/main/java/wcdi/wcdiplayer/widget/DirectoryArrayAdapter.java
+++ app/src/main/java/wcdi/wcdiplayer/widget/DirectoryArrayAdapter.java
@@ -1,7 +1,6 @@
 package wcdi.wcdiplayer.widget;
 
 import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -13,30 +12,17 @@ import wcdi.wcdiplayer.R;
 
 public class DirectoryArrayAdapter extends GenericArrayAdapter<File> {
 
-    private final LayoutInflater inflater;
-
     public DirectoryArrayAdapter(Context context, int resource) {
         super(context, resource);
-
-        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-        mLock = new Object();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        super.getView(position, convertView, parent);
-
-        View view;
-
-        if (convertView == null) {
-            view = inflater.inflate(R.layout.directory_list_item, parent, false);
-        } else {
-            view = convertView;
-        }
+        View view = super.getView(position, convertView, parent);
 
         TextView textView = (TextView) view.findViewById(R.id.directoryName);
+
         try {
             textView.setText(getItem(position).getName());
         } catch (Exception e) {
