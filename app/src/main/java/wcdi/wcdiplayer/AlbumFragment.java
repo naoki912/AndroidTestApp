7b6697087f493ca2c9394877d9c49f063fe49284
Naoki <hnron.reg@gmail.com>
2016-02-07 10:33:24 +0900

    fragment_directory_*をfragment_album_*に変更

--- app/src/main/java/wcdi/wcdiplayer/AlbumFragment.java
+++ app/src/main/java/wcdi/wcdiplayer/AlbumFragment.java
@@ -46,7 +46,7 @@ public class AlbumFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_directory_list, container, false);
+        return inflater.inflate(R.layout.fragment_album_list, container, false);
     }
 
     @Override
